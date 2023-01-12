72f7f223febf76ec70c36e1c58b3220866d7f470
kmoristudent@shacademy.org <kmoristudent@shacademy.org>
2023-01-12 14:25:43 -0800

    working on favorites

--- lib/main.dart
+++ lib/main.dart
@@ -47,9 +47,32 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex =0;
+
+  @override
+
+  
   Widget build(BuildContext context) {
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+      page = GeneratorPage();
+      break;
+      case 1:
+      page = Favoritespage();
+      break;
+      default:
+      throw UnimplementedError('ERROR');
+    }
+
+
     return Scaffold(
       body: Row(
         children: [
@@ -121,6 +144,12 @@ class GeneratorPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class Favoritespage extends StatelessWidget{
+  Widget build(BuildContext context) {
+    return Center(child: Text('Blank Page'));
   }
 }
 
